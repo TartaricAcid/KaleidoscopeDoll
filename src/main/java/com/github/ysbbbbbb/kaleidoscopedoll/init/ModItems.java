3c0c9a0f2088cd7cd5f1f64624db1d7f894b5ff3
tartaric_acid <baka943@qq.com>
2025-03-21 01:56:38 +0800

    增加作者和捐赠者

--- src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/init/ModItems.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/init/ModItems.java
@@ -11,8 +11,6 @@ public final class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(KaleidoscopeDoll.MOD_ID);
 
     public static DeferredItem<Item> DOLL_MACHINE = ITEMS.register("doll_machine", () -> new BlockItem(ModBlocks.DOLL_MACHINE.get(), new Item.Properties()));
-    public static DeferredItem<Item> DOLL_AUTHOR_YSBB = ITEMS.register("doll_0", () -> new DollItem(ModBlocks.DOLL_AUTHOR_YSBB.get()));
-    public static DeferredItem<Item> DOLL_AUTHOR_943 = ITEMS.register("doll_1", () -> new DollItem(ModBlocks.DOLL_AUTHOR_943.get()));
     public static DeferredItem<Item> PURPLE_DOLL_GIFT_BOX = ITEMS.register("purple_doll_gift_box", () -> new BlockItem(ModBlocks.PURPLE_DOLL_GIFT_BOX.get(), new Item.Properties()));
     public static DeferredItem<Item> GREEN_DOLL_GIFT_BOX = ITEMS.register("green_doll_gift_box", () -> new BlockItem(ModBlocks.GREEN_DOLL_GIFT_BOX.get(), new Item.Properties()));
     public static DeferredItem<Item> YELLOW_DOLL_GIFT_BOX = ITEMS.register("yellow_doll_gift_box", () -> new BlockItem(ModBlocks.YELLOW_DOLL_GIFT_BOX.get(), new Item.Properties()));
