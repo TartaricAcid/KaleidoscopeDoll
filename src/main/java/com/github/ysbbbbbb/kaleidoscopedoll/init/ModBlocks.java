d28dc9df5d5a3746992744ab51ced86a7a86308c
tartaric_acid <baka943@qq.com>
2025-03-21 01:56:38 +0800

    增加作者和捐赠者

--- src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/init/ModBlocks.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/init/ModBlocks.java
@@ -1,7 +1,6 @@
 package com.github.ysbbbbbb.kaleidoscopedoll.init;
 
 import com.github.ysbbbbbb.kaleidoscopedoll.KaleidoscopeDoll;
-import com.github.ysbbbbbb.kaleidoscopedoll.block.DollBlock;
 import com.github.ysbbbbbb.kaleidoscopedoll.block.DollGiftBoxBlock;
 import com.github.ysbbbbbb.kaleidoscopedoll.block.DollMachineBlock;
 import com.github.ysbbbbbb.kaleidoscopedoll.event.ModRegisterEvent;
@@ -12,16 +11,12 @@ import net.minecraft.world.level.block.Block;
 
 public class ModBlocks {
     public static final Block DOLL_MACHINE = new DollMachineBlock();
-    public static final Block DOLL_AUTHOR_YSBB = DollBlock.getAuthorDoll();
-    public static final Block DOLL_AUTHOR_943 = DollBlock.getAuthorDoll();
     public static final Block PURPLE_DOLL_GIFT_BOX = new DollGiftBoxBlock();
     public static final Block GREEN_DOLL_GIFT_BOX = new DollGiftBoxBlock();
     public static final Block YELLOW_DOLL_GIFT_BOX = new DollGiftBoxBlock();
 
     public static void registerBlocks() {
         Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(KaleidoscopeDoll.MOD_ID, "doll_machine"), DOLL_MACHINE);
-        Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(KaleidoscopeDoll.MOD_ID, "doll_0"), DOLL_AUTHOR_YSBB);
-        Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(KaleidoscopeDoll.MOD_ID, "doll_1"), DOLL_AUTHOR_943);
         Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(KaleidoscopeDoll.MOD_ID, "purple_doll_gift_box"), PURPLE_DOLL_GIFT_BOX);
         Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(KaleidoscopeDoll.MOD_ID, "green_doll_gift_box"), GREEN_DOLL_GIFT_BOX);
         Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(KaleidoscopeDoll.MOD_ID, "yellow_doll_gift_box"), YELLOW_DOLL_GIFT_BOX);
--- src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/item/DollItem.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/item/DollItem.java
@@ -1,12 +1,26 @@
 package com.github.ysbbbbbb.kaleidoscopedoll.item;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.block.Block;
 
+import java.util.List;
+
 public class DollItem extends BlockItem {
-    public DollItem(Block block) {
+    private final String langKey;
+
+    public DollItem(Block block, String langKey) {
         super(block, new Item.Properties().equipmentSlot((entity, item) -> EquipmentSlot.HEAD));
+        this.langKey = "tooltip.kaleidoscope_doll.doll." + langKey;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> list, TooltipFlag flag) {
+        list.add(Component.translatable(this.langKey).withStyle(ChatFormatting.DARK_GRAY));
     }
 }
