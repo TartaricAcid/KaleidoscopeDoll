ca0bd4f7c5ee85c01609504556146ad598cb5933
tartaric_acid <baka943@qq.com>
2025-03-22 23:34:39 +0800

    - 添加模型分栏 - 现在抽奖机是双方块结构了

--- src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/init/ModCreativeTabs.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopedoll/init/ModCreativeTabs.java
@@ -10,18 +10,32 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import static com.github.ysbbbbbb.kaleidoscopedoll.event.ModRegisterEvent.SPECIAL_TOOLTIPS;
+
 public class ModCreativeTabs {
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, KaleidoscopeDoll.MOD_ID);
-    private static final ResourceLocation ICON_ID = new ResourceLocation(KaleidoscopeDoll.MOD_ID, "doll_52");
+    private static final ResourceLocation VANILLA_ICON_ID = new ResourceLocation(KaleidoscopeDoll.MOD_ID, "doll_52");
+    private static final ResourceLocation PLAYER_ICON_ID = new ResourceLocation(KaleidoscopeDoll.MOD_ID, "doll_0");
 
-    public static RegistryObject<CreativeModeTab> DOLL_TAB = TABS.register("doll", () -> CreativeModeTab.builder()
-            .title(Component.translatable("item_group.kaleidoscope_doll.doll.name"))
-            .icon(() -> ForgeRegistries.ITEMS.getValue(ICON_ID).getDefaultInstance())
+    public static RegistryObject<CreativeModeTab> VANILLA_DOLL_TAB = TABS.register("vanilla_doll", () -> CreativeModeTab.builder()
+            .title(Component.translatable("item_group.kaleidoscope_doll.vanilla_doll.name"))
+            .icon(() -> ForgeRegistries.ITEMS.getValue(VANILLA_ICON_ID).getDefaultInstance())
             .displayItems((par, output) -> {
                 output.accept(ModItems.DOLL_MACHINE.get());
                 output.accept(ModItems.PURPLE_DOLL_GIFT_BOX.get());
                 output.accept(ModItems.GREEN_DOLL_GIFT_BOX.get());
                 output.accept(ModItems.YELLOW_DOLL_GIFT_BOX.get());
-                ModRegisterEvent.DOLL_ITEMS.forEach(output::accept);
+                ModRegisterEvent.DOLL_ITEMS.stream()
+                        .filter(item -> !SPECIAL_TOOLTIPS.containsKey(ForgeRegistries.ITEMS.getKey(item)))
+                        .forEach(output::accept);
+            }).build());
+
+    public static RegistryObject<CreativeModeTab> PLAYER_DOLL_TAB = TABS.register("player_doll", () -> CreativeModeTab.builder()
+            .title(Component.translatable("item_group.kaleidoscope_doll.player_doll.name"))
+            .icon(() -> ForgeRegistries.ITEMS.getValue(PLAYER_ICON_ID).getDefaultInstance())
+            .displayItems((par, output) -> {
+                ModRegisterEvent.DOLL_ITEMS.stream()
+                        .filter(item -> SPECIAL_TOOLTIPS.containsKey(ForgeRegistries.ITEMS.getKey(item)))
+                        .forEach(output::accept);
             }).build());
 }
